1ca972f437f4d00a7053fe260271a5ac6758d407
kiplikipli <zulkifli_000@apps.ipb.ac.id>
2019-12-01 14:19:41 +0700

    tambah main menu sama timer, tapi blom fix

---
+++ Assets/Script/Countdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 3.4f;
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        if(currentTime <= .5f)
+        {
+            SceneManager.LoadScene("Easy");
+        }
+        else
+        {
+            countdownText.text = currentTime.ToString("0");
+        }
+    }
+}
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 30f;
+    [SerializeField] Text timerText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0.00");
+    }
+}
